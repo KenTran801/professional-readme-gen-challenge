03fdb29c21f3701fc1370533b804a22517f56541
KenTran801 <utes.fan.tran@gmail.com>
2021-11-09 22:39:52 -0700

    Working on writeToFile function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [
@@ -44,7 +45,7 @@ const questions = [
             {
                 type: 'input',
                 name: 'contributors',
-                message: 'Please provide the contribtors or how to contribute to your project.',
+                message: 'Please provide the contributors or how to contribute to your project.',
             },
             // Project Tests
             {
@@ -65,18 +66,19 @@ const questions = [
                 name: 'email',
                 message: 'Please provide your email address.',
             },
-
-        ]).catch((error) => {
-            if (error.isTtyError) {
-                // Prompt couldn't be rendered in the current environment
-            } else {
-                // Something else went wrong
-            }
-        })
+        ])
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data,), (err) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success!");
+    }
+}
 
 // TODO: Create a function to initialize app
 function init() { }
